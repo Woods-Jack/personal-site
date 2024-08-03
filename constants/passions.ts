a758cc5ac7f5272bb6a5c2058628ba1851cc5022
Woods-Jack <woodsjack1999@gmail.com>
2024-08-03 15:55:03 +0100

    Added interests

---
+++ constants/passions.ts
@@ -0,0 +1,28 @@
+import { CardRowProps } from "@/components/common/Card/CardRow.type";
+
+export const passions: CardRowProps[] = [
+    {
+      title: 'Formula 1',
+      img: {
+        src: '/f1.jpg',
+        alt: 'A Formula 1 car driving through Milton Keynes.',
+      },
+      desc: 'I am a huge Formula 1 fan and probably the only Alpine fan who will ever visit this site. I grew up only 15 minutes from Silverstone but only really got into the sport in 2021 (what a season!) when a group of my friends started regularly watchin the races together. Fast-forward to now and I was luckily enough to attend my first ever Grand Prix in 2024 in Budapest. This definitely won\'t be the last of my grand prix appearences; I will be looking out for a race in 2025.'
+    },
+    {
+      title: 'Coffee',
+      img: {
+        src: '/coffee.jpg',
+        alt: 'A coffee with latte art sitting on a desk.',
+      },
+      desc: 'I worked from home during lockdown and would make everyone a coffee using my espresso machine every day to keep me sane. The same machine then got me through my final years at University and into my career as a software engineer. I\'ve also picked up other brewers such as the aeropress, moka pot and V60, and love experimenting with these on the weekends. London also has incredible coffee shops which I love to discover whenever I have a free few hours.'
+    },
+    {
+      title: 'Theatre',
+      img: {
+        src: '/theatre.jpg',
+        alt: 'Jack and cast members singing on stage.',
+      },
+      desc: 'I studied GCSE drama and really fell for theatre. I stage managed and lighting designed several productions throughout University, spending most my evenings and weekends in the theatre. In my final year I finally decided to audition for a role in the musical Company and from that moment did every musical theatre event I could until I graduated. I have done some musical theatre courses in London, but would love to get properly back onto the stage with a theatre group soon.'
+    },
+  ];
