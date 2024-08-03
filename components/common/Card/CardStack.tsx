32e9e08b68f8de901b1cbc9c1393e42a1c2315eb
Woods-Jack <woodsjack1999@gmail.com>
2024-08-03 15:55:03 +0100

    Added interests

--- components/common/Card/CardStack.tsx
+++ components/common/Card/CardStack.tsx
@@ -14,7 +14,7 @@ export const CardStack = ({
   return (
     <div className="flex md:flex-row flex-col w-full justify-between bg-light-sec rounded-xl">
       <div className="flex flex-col justify-between md:h-[300px] h-max-[300px] md:w-3/4 w-full py-2 pl-4">
-        <div className="overflow-hidden flex flex-col items-center md:items-start">
+        <div className="overflow-hidden flex flex-col items-center md:items-start md:pr-4">
           <h3>{title}</h3>
           {date && <em>{date}</em>}
           <p>{desc}</p>
