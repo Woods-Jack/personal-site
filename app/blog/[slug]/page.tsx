87a8a600ac46042024246fdb10de506c8184bc34
Jack Woods <jacwoods1@WKMZT4760AEC.global.publicisgroupe.net>
2024-08-26 15:30:35 +0100

    Debug 2 electric boogaloo

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,22 +4,33 @@ import { postPathsQuery, postQuery } from "@/sanity/lib/queries";
 import { sanityFetch } from "@/sanity/lib/sanityFetch";
 import { client } from "@/sanity/lib/client";
 
-// Prepare Next.js to know which routes already exist
 export async function generateStaticParams() {
   // Important, use the plain Sanity Client here
   const posts = await client.fetch(postPathsQuery);
-  if(posts.length > 0) posts[0].params.slug = `blog/${posts[0]?.params?.slug}`;
 
-  return posts ?? null;
+  // Ensure at least one empty object is returned to prevent Vercel build failure
+  return posts.length > 0 ? posts : [{ params: {} }];
 }
 
 export default async function Page({ params }: { params?: any }) {
   const post = await sanityFetch<SanityDocument>({ query: postQuery, params });
-  return(
-  <main className="pt-24">
-     <div className='w-full'>
-     {post ? <Post post={post} /> : <><h1>Page Not Found</h1><div>The page you are looking for doesn&apos;t exist...</div></>}
-     </div>
-  </main>
-  )
+
+  if (!post) {
+    return (
+      <main className="pt-24">
+        <div className="w-full">
+          <h1>Page Not Found</h1>
+          <div>The page you are looking for doesn&apos;t exist...</div>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main className="pt-24">
+      <div className="w-full">
+        <Post post={post} />
+      </div>
+    </main>
+  );
 }
