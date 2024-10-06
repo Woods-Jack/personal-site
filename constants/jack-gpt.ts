22f90659ce599f89811d5259d38d46fd8d3b8daa
Woods-Jack <woodsjack1999@gmail.com>
2024-10-06 15:03:49 +0100

    Link hotfix

--- constants/jack-gpt.ts
+++ constants/jack-gpt.ts
@@ -1,2 +1,2 @@
-export const INIT_MESSAGE = `Welcome to JackGPT. I am an LLM Chatbot that can answer any questions you may have about Jack. You can learn more about how I work <a href="https://www.google.co.uk" target="_blank" rel="noopener noreferrer"><u>here</u></a>.`;
+export const INIT_MESSAGE = `Welcome to JackGPT. I am an LLM Chatbot that can answer any questions you may have about Jack. You can <a href="/blog/jack-gpt" target="_blank"><u>learn more about how I work here</u></a>.`;
 
